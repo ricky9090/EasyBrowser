539e6edb4a9f2fbbd58da60a229b192cb9e18d6c
ucx9002 <unreal_star13@126.com>
2019-04-26 15:14:43 +0800

    代码整理

--- app/src/main/java/ricky/easybrowser/page/browser/BrowserActivity.java
+++ app/src/main/java/ricky/easybrowser/page/browser/BrowserActivity.java
@@ -1,6 +1,5 @@
 package ricky.easybrowser.page.browser;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -84,11 +83,6 @@ public class BrowserActivity extends AppCompatActivity implements NewTabFragment
     }
 
     @Override
-    public void onTabtInteraction(Uri uri) {
-
-    }
-
-    @Override
     public void onTabTitleChanged(String title) {
         tabCacheManager.updateTabTitle();
     }
@@ -98,11 +92,12 @@ public class BrowserActivity extends AppCompatActivity implements NewTabFragment
         if (FragmentBackHandleHelper.isFragmentBackHandled(getSupportFragmentManager())) {
             return;
         }
-        // FIXME
+
         if (tabRecyclerView.getVisibility() == View.VISIBLE) {
             tabRecyclerView.setVisibility(View.GONE);
             return;
         }
+
         super.onBackPressed();
     }
 
