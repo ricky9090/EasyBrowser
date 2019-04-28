1eb45cbe4684209ee267a7081d531fd3404cece9
ucx9002 <unreal_star13@126.com>
2019-04-28 20:36:12 +0800

    添加无图模式切换

--- app/src/main/java/ricky/easybrowser/page/setting/SettingDialog.java
+++ app/src/main/java/ricky/easybrowser/page/setting/SettingDialog.java
@@ -1,11 +1,14 @@
 package ricky.easybrowser.page.setting;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -13,8 +16,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import ricky.easybrowser.R;
+import ricky.easybrowser.utils.SharedPrefenceUtils;
 
 public class SettingDialog extends DialogFragment {
+
+    private CheckBox noPictureMode;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +49,24 @@ public class SettingDialog extends DialogFragment {
                 dismiss();
             }
         });
+
+        noPictureMode = dialogView.findViewById(R.id.no_picture_mode);
+        noPictureMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences sp = SharedPrefenceUtils.getSettingSP(getContext());
+                if (sp == null) {
+                    return;
+                }
+                SharedPreferences.Editor editor = sp.edit();
+                if (isChecked) {
+                    editor.putBoolean(SharedPrefenceUtils.KEY_NO_PIC_MODE, true);
+                } else {
+                    editor.putBoolean(SharedPrefenceUtils.KEY_NO_PIC_MODE, false);
+                }
+                editor.apply();
+            }
+        });
         return dialogView;
     }
 
@@ -52,5 +77,12 @@ public class SettingDialog extends DialogFragment {
         WindowManager.LayoutParams param = getDialog().getWindow().getAttributes();
         param.gravity = Gravity.BOTTOM;
         getDialog().getWindow().setAttributes(param);
+
+        SharedPreferences sp = SharedPrefenceUtils.getSettingSP(getContext());
+        if (sp.getBoolean(SharedPrefenceUtils.KEY_NO_PIC_MODE, false)) {
+            noPictureMode.setChecked(true);
+        } else {
+            noPictureMode.setChecked(false);
+        }
     }
 }
